8d5201c6a54b655c9c0e9707895cdcf936eceb2f
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-11-10 13:26:39 +0100

    Installed styled-components

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import { createGlobalStyle } from "styled-components"
 import { Navbar } from './Navbar'
 
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+  font-family: "Montserrat";
+  color: white;
+}
+html, body {
+  min-height: 100%;
+}
+  body {
+    background: radial-gradient(at top left, rgba(146,43,225,1) 0%, rgba(43,9,107,1) 100%);
+  background-repeat: no-repeat;
+  }
+`
+
 export const Layout = ({ children }) => {
+
+
+
     // allt som layout komponenten wrappar som tex index.js blit childrens till layout komponenten och finns då i props.children
     return (
-        <div>
-            <Navbar />
+        <>
+            <GlobalStyle />
             <div>
-                {children}
+                <Navbar />
+                <div>
+                    {children}
+                </div>
+                <footer>
+                    <p>Copyright 2021 Jimmy Rickardsson</p>
+                </footer>
             </div>
-            <footer>
-                <p>Copyright 2021 Jimmy Rickardsson</p>
-            </footer>
-        </div>
+        </>
     )
 }
