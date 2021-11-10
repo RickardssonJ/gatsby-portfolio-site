d3a52938079027b9685fe35b02f20c62e3e79924
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-11-10 13:31:25 +0100

    Added a new font as the global font

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [`gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Montserrat\:300,400,500,700`,
+        ],
+        display: 'swap'
+      }
+    }
+  ],
 }
