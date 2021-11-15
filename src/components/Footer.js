419b1b931f7bce2d722727c17f828da79bf91829
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-11-15 11:15:41 +0100

    Added rel=noreferrer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import { graphql, Link, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery } from 'gatsby'
 import React from 'react'
 import { StyledFooter } from "../styles/Styling"
 import Img from "gatsby-image"
@@ -7,7 +7,7 @@ import Img from "gatsby-image"
 
 export const Footer = () => {
 
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query footerImg {
         allFile(filter: {relativeDirectory: {eq: "footerImages"}}) {
           edges {
@@ -24,23 +24,23 @@ export const Footer = () => {
       }
     `)
 
-    return (
-        <>
-            <StyledFooter>
-                <p>Copyright 2021 Jimmy Rickardsson</p>
-                <div className="footerImagesDiv">
-                    <a href="https://github.com/RickardssonJ" target="_blank">
-                        <Img className="footerImages" fluid={data.allFile.edges[0].node.childImageSharp.fluid} />
-                    </a>
+  return (
+    <>
+      <StyledFooter>
+        <p>Copyright 2021 Jimmy Rickardsson</p>
+        <div className="footerImagesDiv">
+          <a href="https://github.com/RickardssonJ" target="_blank" rel="noreferrer">
+            <Img className="footerImages" fluid={data.allFile.edges[0].node.childImageSharp.fluid} />
+          </a>
 
-                    <a href="https://www.linkedin.com/in/jimmy-rickardsson-829072173/" target="_blank">
-                        <Img className="footerImages" fluid={data.allFile.edges[1].node.childImageSharp.fluid} />
-                    </a>
-                    <a href="mailto: rickardssonjimmy@gmail.com" target="_blank">
-                        <Img className="footerImages" fluid={data.allFile.edges[2].node.childImageSharp.fluid} />
-                    </a>
-                </div>
-            </StyledFooter>
-        </>
-    )
+          <a href="https://www.linkedin.com/in/jimmy-rickardsson-829072173/" target="_blank" rel="noreferrer">
+            <Img className="footerImages" fluid={data.allFile.edges[1].node.childImageSharp.fluid} />
+          </a>
+          <a href="mailto: rickardssonjimmy@gmail.com" target="_blank" rel="noreferrer">
+            <Img className="footerImages" fluid={data.allFile.edges[2].node.childImageSharp.fluid} />
+          </a>
+        </div>
+      </StyledFooter>
+    </>
+  )
 }
