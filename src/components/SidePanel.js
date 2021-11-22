a7a5dc2ea87fc5694c0892d0c1c2e4bcbc9e9c0f
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-11-22 14:03:40 +0100

    Refactored code

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -4,7 +4,7 @@ import { graphql, useStaticQuery } from 'gatsby'
 import { StyledSidePanel } from "../styles/Styling"
 
 export const SidePanel = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query sidePanelImg {
         allFile(filter: {relativeDirectory: {eq: "footerImages"}}) {
           edges {
@@ -21,24 +21,24 @@ export const SidePanel = () => {
       }
     `)
 
-    return (
+  return (
 
 
-        <StyledSidePanel>
-            <div className="footerImagesDiv">
-                <a href="https://github.com/RickardssonJ" target="_blank" rel="noreferrer">
-                    <Img className="footerImages" fluid={data.allFile.edges[0].node.childImageSharp.fluid} />
-                </a>
+    <StyledSidePanel>
+      <div className="footerImagesDiv">
+        <a href="https://github.com/RickardssonJ" target="_blank" rel="noreferrer">
+          <Img className="footerImages" fluid={data.allFile.edges[0].node.childImageSharp.fluid} />
+        </a>
 
-                <a href="https://www.linkedin.com/in/jimmy-rickardsson-829072173/" target="_blank" rel="noreferrer">
-                    <Img className="footerImages" fluid={data.allFile.edges[1].node.childImageSharp.fluid} />
-                </a>
-                <a href="mailto: rickardssonjimmy@gmail.com" target="_blank" rel="noreferrer">
-                    <Img className="footerImages" fluid={data.allFile.edges[2].node.childImageSharp.fluid} />
-                </a>
-            </div>
-        </StyledSidePanel>
+        <a href="https://www.linkedin.com/in/jimmy-rickardsson-829072173/" target="_blank" rel="noreferrer">
+          <Img className="footerImages" fluid={data.allFile.edges[1].node.childImageSharp.fluid} />
+        </a>
+        <a href="mailto: rickardssonjimmy@gmail.com" target="_blank" rel="noreferrer">
+          <Img className="footerImages" fluid={data.allFile.edges[2].node.childImageSharp.fluid} />
+        </a>
+      </div>
+    </StyledSidePanel>
 
 
-    )
+  )
 }
